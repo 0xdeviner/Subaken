6d1615050f80f00e75ab1af4fe595f496681267f
0xdeviner <iamdeviner@gmail.com>
2021-03-21 14:01:00 +0530

    initial

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+sudo apt-get update;
+sudo apt-get upgrade -y;
+sudo apt install -y libpcap-dev;
+sudo apt-get install python3 -y;
+sudo apt-get install python3-pip -y;
+sudo apt-get install -y python-pip -y;
+sudo apt-get install ruby -y;
+sudo apt-get install screen -y;
+sudo apt-get install git -y;
+sudo pip install discord-webhook;
+
+mkdir ~/Subtaken/subtakeover;
+
+#install go
+echo "${red}Installing Golang${reset}"
+sudo wget https://golang.org/dl/go1.16.2.linux-amd64.tar.gz;
+sudo tar -xvf go1.16.2.linux-amd64.tar.gz;
+sudo mv go /usr/local;
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go'	>> ~/.bashrc			
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+sudo rm -rf go1.16.2.linux-amd64.tar.gz;
+sleep 2
+echo "${green}Done...!!${reset}"
+
+
+cd ~/Subtaken/subtakeover;
+git clone https://github.com/arkadiyt/bounty-targets-data;
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+go get github.com/Ice3man543/SubOver;
+cd ~/go/bin;
+wget https://raw.githubusercontent.com/Ice3man543/SubOver/master/providers.json; 
+cd ~/
+
+source ~/.bashrc
+
+cd ~/Subtaken/
+chmod +x *
+cd ~/
+## make sure to go here and update username pass for bellow listed services $HOME/.config/subfinder/config.json
+
+##virustotal.com
+##community.riskiq.com
+##securitytrails.com
+##riddler.io
+##censys.io
+##shodan.io
+##github.com
---
+++ subtaken.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+dir=`pwd`; 
+cd ~/Subtaken/subtakeover/bounty-targets-data/; 
+git pull; 
+cd $dir; 
+cp ~/Subtaken/subtakeover/bounty-targets-data/data/wildcards.txt ./; cat wildcards.txt | sed 's/^*.//g' | grep -v '*' > wildcards_without_stars.txt; 
+while read host; 
+   do file=$host && file+="_subfinder.out"; 
+   ~/go/bin/subfinder -o $file -d $host; 
+done < ./wildcards_without_stars.txt
+
+
+cat ./*.out > all_subdomains.lst; 
+~/go/bin/SubOver -l ./all_subdomains.lst -timeout 5 -o subover.out;
+
+python ~/Subtaken/notify.py
